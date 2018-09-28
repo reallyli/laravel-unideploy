a457da12911bde2ca40e7ae7b4a545650e0080ac
reallyli <zlireallyli@outlook.com>
2018-09-28 23:34:53 +0800

    add deploy final execution file

---
+++ tests/.build/deploy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel-deployer.php';
+
+/*
+ * Includes
+ */
+
+/*
+ * Options
+ */
+
+set('strategy', 'basic');
+set('application', 'LaravelDeployer');
+set('keep_releases', 6);
+set('php_fpm_service', 'php7.2-fpm');
+set('group_notify', false);
+set('notify_channel_url', null);
+set('repository', null);
+set('shared_dirs', null);
+
+/*
+ * Hosts and localhost
+ */
+
+/*
+ * Strategies
+ */
+
+/*
+ * Hooks
+ */
+
+after('hook:ready', 'artisan:view:clear');
+after('hook:ready', 'artisan:cache:clear');
+after('hook:ready', 'artisan:config:cache');
+after('hook:ready', 'artisan:optimize');
+after('hook:done', 'fpm:reload');
+after('success', 'record:revision:log');
