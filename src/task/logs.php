cab1be0122997f903911c6331d62d8fd0e0e6c37
reallyli <zlisreallyli@outlook.com>
2019-01-30 18:16:29 +0800

    添加 log 配置

--- src/task/logs.php
+++ src/task/logs.php
@@ -2,8 +2,15 @@
 
 namespace Deployer;
 
-set('log_lines', 200);
-set('log_command', 'cat storage/logs/laravel.log | grep -Ev "^#[[:digit:]]|^\[stacktrace\]$|^\"\}$" | tail -n {{log_lines}}');
+set('log_lines', function () {
+    return get('log_line', 200);
+});
+
+set('log_file_name', function () {
+    return get('log_file_name', 'laravel.log');
+});
+
+set('log_command', 'cat storage/logs/{{log_file_name}} | grep -Ev "^#[[:digit:]]|^\[stacktrace\]$|^\"\}$" | tail -n {{log_lines}}');
 
 desc('Read logs from a given host');
 task('logs', function () {
